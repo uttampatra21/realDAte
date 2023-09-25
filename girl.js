6ddb4ccf3993fb8a443cce1f3ddb4de1d9d5ee27
uttam kumar patra <dev.uttampatra@gmail.com>
2023-09-25 13:40:05 +0530

    0

--- girl.js
+++ girl.js
@@ -1,11 +1,23 @@
 const girlSec = document.querySelector("#girls");
 const x = document.querySelector(".x");
-
-const girl1 = () => {
-  document.querySelector(".girl-sec").style.visibility = "visible";
-  console.log("hi");
-};
-
 x.addEventListener("click", () => {
   document.querySelector(".girl-sec").style.visibility = "hidden";
 });
+
+const girl1 = () => {
+  document.querySelector(".girl-sec").style.visibility = "visible";
+  document.querySelector("#changeGirl").src =
+    "/images/section1/WhatsApp Image 2023-09-25 at 12.33.34.jpeg";
+};
+
+const girl2 = () => {
+  document.querySelector(".girl-sec").style.visibility = "visible";
+  document.querySelector("#changeGirl").src =
+    "/images/section1/WhatsApp Image 2023-09-25 at 12.33.35 (1).jpeg";
+};
+
+const girl3 = () => {
+  document.querySelector(".girl-sec").style.visibility = "visible";
+  document.querySelector("#changeGirl").src =
+    "http://127.0.0.1:5500/images/section2/WhatsApp%20Image%202023-09-25%20at%2012.33.50.jpeg";
+};
